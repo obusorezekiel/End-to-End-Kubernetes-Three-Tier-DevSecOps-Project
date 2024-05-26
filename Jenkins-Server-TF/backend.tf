21818f3d3cd575fa3228bce3d3a5df90d464dc26
Ezekiel Umesi <obusorezekiel@gmail.com>
2024-05-26 23:25:25 +0100

    Test

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,15 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "ezekiel-module-bucket"
     region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    key            = "Jenkins-Server-TF/terraform.tfstate"
   }
-  required_version = ">=0.13.0"
   required_providers {
     aws = {
-      version = ">= 2.7.0"
       source  = "hashicorp/aws"
     }
   }
